2914ec8f0db0db9f45f293d8b5c26270b6a5f894
Vasil Stanoev <vasilstanoev94@gmail.com>
2019-12-12 18:31:15 +0200

    Add files via upload

---
+++ Bank-Account-Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using BankAccountProgram;
+using System;
+
+namespace Bank_Account_Tests
+{
+    [TestFixture] //Attribute that marks a class that contains tests
+    public class BankAccountTests
+    {
+        [Test] //Test method
+        public void MakeDepositTestOne()
+        {
+            var acc = new BankAccount(1000);
+
+            acc.Deposit(1000);
+
+            Assert.AreEqual(2000, acc.Amount);
+        }
+        [Test]
+
+        public void MakeDepoditTestTwo()
+        {
+            var acc = new BankAccount(0);
+
+            acc.Deposit(0);
+
+            Assert.AreEqual(0, ;
+        }
+    }
+}
