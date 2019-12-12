004d85058f695f50723502fc5def414d2a7abe21
Vasil Stanoev <vasilstanoev94@gmail.com>
2019-12-12 18:32:12 +0200

    Add files via upload

---
+++ StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SeleniumBasicDemo
+{
+    class StartUp
+    {
+        public static void Main()
+        {
+            BankAccount acc = new BankAccount(1000);
+
+            BankAccount secondacc = new BankAccount(3000);
+
+            acc.Deposit(5000);
+
+            secondacc.Deposit(30000);
+
+            secondacc.Withdraw(200);
+
+            System.Console.WriteLine(acc.Amount);
+
+            System.Console.WriteLine(secondacc.Amount);
+        }
+    }
+}
